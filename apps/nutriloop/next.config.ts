1800693825c8e6ddcd7df4be6677e15ad055be78
telephant11 <telephant11@gmail.com>
2025-06-17 18:14:58 +0400

    fix: Next.config

--- apps/nutriloop/next.config.ts
+++ apps/nutriloop/next.config.ts
@@ -1,10 +1,33 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Empty config for now
-  output: 'standalone',
-  experimental: {
-  },
-};
+const deployment = process.env.DEPLOYMENT || 'vercel';
+
+let nextConfig: NextConfig = {}
+switch (deployment) {
+  case 'vercel':
+    nextConfig = {
+      output: 'standalone',
+    }
+    break;
+  case 'github':
+    nextConfig = {
+      output: 'export', // enable static export
+      basePath: '/tbox', // use repository name as sub-path
+      assetPrefix: '/tbox/',
+
+      images: {
+        unoptimized: true, // close image optimization
+      },
+    }
+    break;
+  case 'cloudflare':
+    nextConfig = {
+      output: 'export', // enable static export
+      images: {
+        unoptimized: true, // close image optimization
+      },
+    }
+    break;
+}
 
 export default nextConfig;
